bafbd9eee4ea454b6923314856e863afd4490b8e
ChristopherPHolder <toholderandrews@gmail.com>
2024-07-09 20:18:58 +0200

    fix: wip

--- packages/utils-lib/src/lib/chunk.utils.ts
+++ packages/utils-lib/src/lib/chunk.utils.ts
@@ -1,6 +1,11 @@
 import { Metafile } from 'esbuild';
+import { ReadonlyDeep, PickIndexSignature } from 'type-fest'
 
-export function importsInEntryPoint(entryPoint: string, metaFileOutputs: Metafile['outputs'], traversedImports = [entryPoint]): string[] {
+export function importsInEntryPoint(
+    entryPoint: string,
+    metaFileOutputs: ReadonlyDeep<Metafile['outputs']>,
+    traversedImports: readonly string[] = [entryPoint]
+): readonly string[] {
   const staticImports = metaFileOutputs[entryPoint].imports.filter(
       ({ kind, path }) => kind !== 'dynamic-import' && !traversedImports.includes(path),
   );
@@ -12,11 +17,47 @@ export function importsInEntryPoint(entryPoint: string, metaFileOutputs: Metafil
   return staticImports.flatMap(({ path }) => importsInEntryPoint(path, metaFileOutputs, [...traversedImports, path]));
 }
 
+export function getChunkNameByEntryPoint(entryPoint: string, metafileOutputs: ReadonlyDeep<Metafile['outputs']>): string | undefined {
+  return Object.keys(metafileOutputs).find((name) => metafileOutputs[name].entryPoint === entryPoint);
+}
 
-export function getChunkNameByEntryPoint(entryPoint: string, metafileOutputs: Metafile['outputs']) {
-  const chunkName = Object.keys(metafileOutputs).find((name) => metafileOutputs[name].entryPoint === entryPoint);
-  if (!chunkName) {
-    throw new Error(`Unable to find ${entryPoint} entryPoint`);
+export function mergeOutputs(metaFileOutputs: ReadonlyDeep<Metafile['outputs']>, targets: readonly string[]): ReadonlyDeep<Metafile['outputs']> {
+  const targetsOutput = targets.map((target) => metaFileOutputs[target]).reduce(mergeTargetOutputs);
+
+  return { targetsOutput };
+}
+
+function mergeTargetOutputs(
+    mergedOutputs: ReadonlyDeep<Metafile['outputs'][string]>,
+    currentTarget: ReadonlyDeep<Metafile['outputs'][string]>
+): ReadonlyDeep<Metafile['outputs'][string]> {
+  return {
+    bytes: mergedOutputs.bytes + currentTarget.bytes,
+    inputs: {}, // @TODO
+    imports: mergeImports(mergedOutputs.imports, currentTarget.imports),
+    exports: [...mergedOutputs.exports, ...currentTarget.exports],
+    // @TODO There does not seem to be any logical way to handle merging entry points, in theory this should be able to happen.
+    entryPoint: mergedOutputs.entryPoint || currentTarget.entryPoint,
+    cssBundle: mergedOutputs.cssBundle || currentTarget.cssBundle,
   }
-  return chunkName;
+}
+
+function mergeImports(
+    mergedOutputsImports: ReadonlyDeep<Metafile['outputs'][string]['imports']>,
+    currentOutputsImports: ReadonlyDeep<Metafile['outputs'][string]['imports']>
+): ReadonlyDeep<Metafile['outputs'][string]['imports']> {
+  return [...mergedOutputsImports, ...currentOutputsImports].reduce((mergedImports, currentImport) => {
+    const matchingImport = mergedImports.find(({path}) => path !== currentImport.path);
+    return [...mergedImports, !matchingImport ? currentImport : mergedMatchingImports(currentImport, matchingImport)];
+  }, [] as ReadonlyDeep<Metafile['outputs'][string]['imports']>);
+}
+
+function mergedMatchingImports(
+    current: ReadonlyDeep<Metafile['outputs'][string]['imports'][number]>,
+    match: ReadonlyDeep<Metafile['outputs'][string]['imports'][number]>
+): ReadonlyDeep<Metafile['outputs'][string]['imports'][number]> {
+  const kindMatch = current.kind === match.kind && current.kind;
+  const isStatement = [current.kind, match.kind].includes('import-statement') && 'import-statement';
+  const isDynamic = [current.kind, match.kind].includes('dynamic-import') && 'dynamic-import';
+  return { path: current.path || match.path, kind: kindMatch || isStatement || isDynamic || current.kind, external: current.external || match.external }
 }
