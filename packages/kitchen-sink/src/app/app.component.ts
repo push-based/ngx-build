e390555a078664821e3ee97b62118c42d3e9f35b
ChristopherPHolder <toholderandrews@gmail.com>
2024-07-09 13:08:20 +0200

    fix: add lazy features to demo

--- packages/kitchen-sink/src/app/app.component.ts
+++ packages/kitchen-sink/src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { Ui1Component } from "../shared/ui/ui-1.component";
+import { Ui2Component } from "../shared/ui/ui-2.component";
+import { Ui3Component } from "../shared/ui/ui-3.component";
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [RouterModule, Ui1Component, Ui2Component, Ui3Component],
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  template: `
+    <h1>App Root</h1>
+    <app-ui-1 />
+    <app-ui-2 />
+    <app-ui-3 />
+    <router-outlet />
+  `,
 })
 export class AppComponent {
   title = 'kitchen-sink';
--- packages/kitchen-sink/src/app/app.routes.ts
+++ packages/kitchen-sink/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    {
+        path: '1',
+        loadComponent: () => import('../shared/feature/feature-1.component')
+    },
+    {
+        path: '2',
+        loadComponent: () => import('../shared/feature/feature-2.component')
+    },
+    {
+        path: '3',
+        loadComponent: () => import('../shared/feature/feature-3.component')
+    }
+];
