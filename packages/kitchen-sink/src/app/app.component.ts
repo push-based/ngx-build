b3351a3cd6b1295940bd27c4f016528bc7a0eb50
ChristopherPHolder <toholderandrews@gmail.com>
2025-09-11 10:42:21 +0200

    feat: extend demo

--- packages/kitchen-sink/src/app/app.component.ts
+++ packages/kitchen-sink/src/app/app.component.ts
@@ -1,18 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { Ui1Component } from "../shared/ui/ui-1.component";
-import { Ui2Component } from "../shared/ui/ui-2.component";
-import { Ui3Component } from "../shared/ui/ui-3.component";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   standalone: true,
-  imports: [RouterModule, Ui1Component, Ui2Component, Ui3Component],
+  imports: [RouterOutlet],
   selector: 'app-root',
   template: `
-    <h1>App Root</h1>
-    <app-ui-1 />
-    <app-ui-2 />
-    <app-ui-3 />
+    <h1>Ngx Build Demo</h1>
+
     <router-outlet />
   `,
 })
--- packages/kitchen-sink/src/app/app.routes.ts
+++ packages/kitchen-sink/src/app/app.routes.ts
@@ -2,15 +2,7 @@ import { Route } from '@angular/router';
 
 export const appRoutes: Route[] = [
     {
-        path: '1',
-        loadComponent: () => import('../shared/feature/feature-1.component')
+        path: '',
+        loadChildren: () => import('../shared/chunkers/root-chunker.routes').then(m => m.rootChunkerRoutes),
     },
-    {
-        path: '2',
-        loadComponent: () => import('../shared/feature/feature-2.component')
-    },
-    {
-        path: '3',
-        loadComponent: () => import('../shared/feature/feature-3.component')
-    }
 ];
